0f24a41f89988aba795eff15df1f382d7b5e45a0
shuto <giaiwriting32@gmail.com>
2025-07-10 11:30:23 +0900

    通知

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,7 @@
 import type { NextConfig } from 'next';
-import withPWAInit from '@ducanh2912/next-pwa';
-
-const withPWA = withPWAInit({
-  dest: 'public',
-  // カスタムService Workerを指定
-  workboxOptions: {
-    swSrc: 'src/sw.js', // カスタムSWのソース
-    swDest: 'public/sw.js', // 出力先
-    mode: 'production',
-
-    // 自動生成ファイルの除外
-    exclude: [/\.map$/, /manifest$/, /\.DS_Store$/, /^workbox-.*\.js$/],
-  },
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
