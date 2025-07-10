5e509ef7e856e2ee5f2fe0476a932590bd013ddd
shuto <giaiwriting32@gmail.com>
2025-07-10 11:22:04 +0900

    自動ファイル生成制御

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,15 @@ import withPWAInit from '@ducanh2912/next-pwa';
 
 const withPWA = withPWAInit({
   dest: 'public',
+  // カスタムService Workerを指定
+  workboxOptions: {
+    swSrc: 'src/sw.js', // カスタムSWのソース
+    swDest: 'public/sw.js', // 出力先
+    mode: 'production',
+
+    // 自動生成ファイルの除外
+    exclude: [/\.map$/, /manifest$/, /\.DS_Store$/, /^workbox-.*\.js$/],
+  },
 });
 
 const nextConfig: NextConfig = {
